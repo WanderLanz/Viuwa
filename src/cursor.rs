d780cfd12b2bafb9787f935a5fd058091debe338
lwarden1 <lwarden1@trinity.edu>
2023-02-10 15:04:38 -0600

    fix prompt insert

--- src/cursor.rs
+++ src/cursor.rs
@@ -237,8 +237,8 @@ impl AsciiPrompt {
             } else {
                 self.buf.insert(self.idx(), c);
             }
-            _execute!(term, write_as(&self.buf[self.idx()..]), cursor_to_col(self.cur));
             self.cur += 1;
+            _execute!(term, write_as(&self.buf[self.idx()..]), cursor_to_col(self.cur));
         }
     }
     /// Returns the end index of the current word segment (exclusive), including end of string.
